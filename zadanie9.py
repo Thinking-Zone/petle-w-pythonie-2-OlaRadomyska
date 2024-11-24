17de3520c9c7077f874b04fad1fe0f879f3f22a8
OlaRadomyska <olaradomyska@gmail.com>
2024-11-24 18:49:39 +0100

    Update zadanie9.py

---
+++ zadanie9.py
@@ -0,0 +1,11 @@
+def znajdz_pierwsza_podzielna():
+  # Przechodzimy przez liczby od 1 do 100
+  for liczba in rage (1, 101):
+    if liczba % 7 == 0: # Sprawdzamy, czy liczba jest podzielona przez 7 
+    print(f"Pierwsza liczba podzielona przez 7 to : {liczba})
+    break # Zatrzymuje pętle po znalezieniu pierwszej liczby podzielnej przez 7
+
+    # Uruchamiamy program
+    znajdz_pierwsz__podzielna()
+
+Pierwsza liczba podzielna przez 7 to: 7
